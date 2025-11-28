3d286f6289b5bc8d4fec85668968a2f9ac6ae460
vietm1a <vietm1a4@gmail.com>
2025-11-28 21:27:56 +0700

    fix address

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ async def startup():
     db = await get_db()
     
     await db.listings.create_index([("location", "2dsphere")])
-    await db.listings.create_index([("title", "text"), ("desc", "text")])
+    await db.listings.create_index([("title", "text"), ("desc", "text"), ("address", "text")])
     await db.users.create_index("email", unique=True)
     await db.profiles.create_index([("user_id", 1)], unique=True)
     await db.profiles.create_index([("budget", 1)])
